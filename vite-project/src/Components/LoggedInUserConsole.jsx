c97e429cc82d7743c88c942779c19e9ee0013ae5
Deekay <deepakkailash.balamurugan@gmail.com>
2025-04-02 23:05:19 +0530

    Lifted the DisplayText and Content state up to the LoggedInUserConsole for better customization of the console

--- vite-project/src/Components/LoggedInUserConsole.jsx
+++ vite-project/src/Components/LoggedInUserConsole.jsx
@@ -2,6 +2,19 @@ import {useState, useEffect} from 'react'
 import TheInAppNavigtor from './TheInAppNavigator.jsx'
 
 const LoggedInUserConsole = ({Navigator})=>{
+             const [whattodisplay,setwhattodisplay]= useState('Ask a question')
+            const [content, setcontent] = useState(null)
+            const changeContent = ()=>{
+                    setcontent(whattodisplay)
+    }
+    const changewhattodisplay = ()=>{
+        if(whattodisplay=='Ask a question'){
+            setwhattodisplay('Answer a question')
+        }
+        else{
+             setwhattodisplay('Ask a question')
+        }
+    }
 
         const fetchcheck = async ()=>{
                  let res = await fetch('http://127.0.0.1:5000/everyrequestcheck',{
@@ -28,13 +41,19 @@ const LoggedInUserConsole = ({Navigator})=>{
         },[])
 
 
-        return(
-            <div className='bg-black flex flex-col items-center justify-end h-[100vh] w-[100vw]'>
-                    <TheInAppNavigtor />
-            </div>
+                return !content?
+                (
+                <div className='bg-black flex flex-col items-center justify-end h-[100vh] w-[100vw]'>
+                    <TheInAppNavigtor whattodisplay={whattodisplay} changewhattodisplay={changewhattodisplay} changeContent={changeContent} />
+            </div>):(<></>)
 
 
-        )
+
+
+
+
+
+
 
 }
 
